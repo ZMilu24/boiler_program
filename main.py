e88e91a35060654ffa448c48a4ae56233fa9547c
ZMilu24 <93102357+ZMilu24@users.noreply.github.com>
2023-12-19 14:34:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-{\rtf1}
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/termometer-input")
+def termometer_input():
+    termometer=request.get_json()["termoValue"]
+    return(jsonify({"termometerValue" : termometer}))
+
+@app.route("/user-input")
+def user_input():
+    user=request.get_json()["userValue"]
+    return(jsonify({"userValue" : user}))
+
+@app.route("/get-data")
+def get_data():
+    data=request.get_json()["termoValue"]
+    return(jsonify({"userValue" : data}))
+
+if (__name__ == "__main__") :
+    app.run(debug=True)
