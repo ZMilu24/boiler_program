fb7e00c636d6617c6cab4405648ff73835bf2abf
ZMilu24 <93102357+ZMilu24@users.noreply.github.com>
2023-12-21 14:52:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from database import SQL
 
 app = Flask(__name__)
 AB=SQL()
+
+def cmd(line):
+    AB.command(line)
+    print(line)
+
 try:
     AB.create_database("boiler")
     print("Database created")
@@ -17,44 +22,60 @@ try:
     print("log table created")
 except:
     print("log table exists")
-try:
-    AB.create_table("rt_temp (active BOOL, userValue INT, meterValue INT)")
-    print("rt_temp table created")
-except:
-    print("rt_temp table exists") #rt_temp means real time temperature
-asd="INSERT INTO rt_temp (active, userValue, meterValue) VALUES (0, 0, 0)" # meterValue is termometer value
-AB.command(asd)
+
+
+def rt_temp_check():
+    AB.connect("boiler")
+    try:
+        AB.create_table("rt_temp (active BOOL, userValue INT, meterValue INT)") # meterValue is termometer value
+        print("rt_temp table created")
+    except:
+        print("rt_temp table exists") #rt_temp means real time temperature
+    try:
+        data=AB.get_all("rt_temp")
+        if (data[0][1] > data[0][2]):
+            return("rt_record does excist")
+    except:
+        AB.create_rt_temp_record()
 
 def check():
+    rt_temp_check()
     data=AB.get_all("rt_temp")
-    if (data[0][1] > data[0][2]):
-        cmd="UPDATE rt_temp SET active = 1"
+    print(data)
+    if (AB.get_user() < AB.get_meter()):
+        line="UPDATE rt_temp SET active = 1;"
+        AB.set_active(1)
     else:
-        cmd="UPDATE rt_temp SET active = 0"
-    AB.command(cmd)
+        line="UPDATE rt_temp SET active = 0;"
+        AB.set_active(0)
+    cmd(line)
 
 def data_getter(tp): #tp means type
+    rt_temp_check()
     data=request.get_json()[tp]
-    cmd="UPDATE rt_temp SET "+tp+" = "+str(data)
-    print(cmd)
-    AB.command(cmd)
-    #check()
+    line="UPDATE rt_temp SET "+tp+" = "+str(data)
+    print(line)
+    cmd(line)
+    check()
     return(data)
 
 @app.route("/termometer-input", methods=["POST"])
 def termometer_input(): #input json{"meterValue" : 22}
     termometer=data_getter("meterValue")
-    cmd="INSERT INTO log (currentTime, meterValue) VALUES ('"+str(datetime.now().strftime("%H.%M.%S"))+"', "+str(termometer)+" )" # meterValue is termometer value
-    AB.command(cmd)
+    AB.set_meter(termometer)
+    line="INSERT INTO log (currentTime, meterValue) VALUES ('"+str(datetime.now().strftime("%H.%M.%S"))+"', "+str(termometer)+" )" # meterValue is termometer value
+    cmd(line)
     return(jsonify({"termometerValue" : termometer}))
 
 @app.route("/user-input", methods=["POST"])
 def user_input(): #input json{"userValue" : 22}
     user=data_getter("userValue")
+    AB.set_user(user)
     return(jsonify({"userValue" : user}))
 
 @app.route("/get-data")
 def get_data():
+    rt_temp_check()
     data=AB.get_all("rt_temp")
     print(data)
     return(jsonify({"Status" : data[0][0], "User value" : data[0][1], "Termometer value" : data[0][2]}))
