8d2f0645cf02e234b13c77bd12b19323aa4100fb
ZMilu24 <93102357+ZMilu24@users.noreply.github.com>
2023-12-21 14:52:12 +0100

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import mysql.connector
 
 class SQL():
     def command(self, com):
-        return(self.mycursor.execute(com))
+        self.mycursor.execute(com)
     
     def get_all(self, tablename):
         self.command("SELECT * FROM "+tablename)
@@ -30,6 +30,26 @@ class SQL():
     def create_table(self, nameAndData):
         self.command("CREATE TABLE "+nameAndData)
 
+    def create_rt_temp_record(self):
+        cmd="INSERT INTO rt_temp (active, userValue, meterValue) VALUES ("+str(self.active)+", "+str(self.userValue)+", "+str(self.meterValue)+")"
+        self.command(cmd)
+
+    def set_user(self, val):
+        self.userValue=val
+    def set_meter(self, val):
+        self.meterValue=val
+    def set_active(self, val):
+        self.active=val
+    def get_user(self):
+        return self.userValue
+    def get_meter(self):
+        return self.meterValue
+    def get_active(self):
+        return self.active
+
     def __init__(self):
         self.mydb=None
         self.mycursor=None
+        self.userValue=0
+        self.meterValue=0
+        self.active=0
